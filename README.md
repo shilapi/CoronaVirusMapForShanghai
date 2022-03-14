9423b059a29a230223a2d6c94ff4a1923f6375b2
shilapi <shilapi@outlook.com>
2022-03-14 10:08:37 +0800

    修正了高德api地址查询不添加城市关键词导致的定位错误问题

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # CoronaVirusMapForShanghai
 
-##[上海本土疫情地图（点）](maplab.amap.com/share/mapv/1cb6f880670c766d3838b224f751638f)
+##[上海本土疫情地图（点）](http://maplab.amap.com/share/mapv/1cb6f880670c766d3838b224f751638f)
 
-##[上海本土疫情地图（热力图）](maplab.amap.com/share/mapv/8664591567963c3fb31c5bb13b2c0d5c)
+##[上海本土疫情地图（热力图）](http://maplab.amap.com/share/mapv/8664591567963c3fb31c5bb13b2c0d5c)
--- virusmap_main.py
+++ virusmap_main.py
@@ -60,13 +60,16 @@ def mainProgess(virusPlaceListInput):
     virusPlaceList.append(findKeyWord(virusPlaceTmp, '居住于(.+?)，'))
 
     # 写入地址
+    virusPlaceListOutShanghai = []
     virusPlaceListOut = np.unique(sum(sum(virusPlaceList, []), []))
-    print('outputStart' + str(virusPlaceListOut))
-    sheet1.range('a' + placeStart).options(transpose=True).value = virusPlaceListOut
+    for i in virusPlaceListOut :
+        virusPlaceListOutShanghai.append('上海市'+i)
+    print('outputStart' + str(virusPlaceListOutShanghai))
+    sheet1.range('a' + placeStart).options(transpose=True).value = virusPlaceListOutShanghai
 
     # 查找经纬度并写入
     viruslocationList = []
-    for address in virusPlaceListOut:
+    for address in virusPlaceListOutShanghai:
         locationChache = GaodeLocationGet.location(address, '4050c5266eaba083b4dda056ae8d3633')  # Token为个人token，请勿滥用
         if locationChache == None:
             locationChache = ['', '']
